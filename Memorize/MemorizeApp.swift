a1928a83527338d878d5884dc38905f4087e2af8
Rudi Farkas <rudi.farkas@gmail.com>
2021-09-03 14:16:22 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Rudolf Farkas on 03.09.21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
